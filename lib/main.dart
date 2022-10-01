2afd7785ec13f2de9a4d8de3ff44da7cf7232b67
JamesonCrate <jamesoncrate@gmail.com>
2022-10-01 16:14:19 -0700

    Checkpoint 10/1 - Worked on homescreen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Food {
@@ -9,12 +11,15 @@ class Food {
   Food(this.name, this.image);
 }
 
+var rng = Random();
+
 class DiningHall {
   late String name;
   late String image;
   late double allRating;
   late List menu;
-  double dailyRating = 3;
+  bool isOpen = rng.nextBool();
+  int dailyRating = rng.nextInt(5)+1;
 
   DiningHall(this.name, this.image);
 
@@ -30,21 +35,40 @@ class DiningHall {
 }
 
 class Review {
+  late String location;
   late double rating;
   late List meal;
   late String comment;
 
-  Review(this.rating, this.meal, this.comment);
+  Review(this.location, this.rating, this.meal, this.comment);
 }
 
-var cafeThree = DiningHall('Cafe 3', '');
-var clarkKerr = DiningHall('Clark Kerr', '');
-var foothill = DiningHall('Foothill', '');
-var crossroads = DiningHall('Crossroads', '');
+DiningHall cafeThree = DiningHall('Cafe 3', '');
+DiningHall clarkKerr = DiningHall('Clark Kerr', '');
+DiningHall foothill = DiningHall('Foothill', '');
+DiningHall crossroads = DiningHall('Crossroads', '');
 
-var chosenHall = cafeThree;
+List<DiningHall> sortHalls(List<DiningHall> options) {
+  List<DiningHall> openHalls = [];
+  List<DiningHall> closedHalls = [];
+  for (DiningHall h in options) {
+    if (h.isOpen) {
+      openHalls.add(h);
+    } else {
+      closedHalls.add(h);
+    }
+  }
+  closedHalls.sort((a, b) => b.dailyRating.compareTo(a.dailyRating));
+  openHalls.sort((a, b) => b.dailyRating.compareTo(a.dailyRating));
+  return openHalls + closedHalls;
+}
 
-List<DiningHall> halls = [clarkKerr, cafeThree, foothill, crossroads];
+String dailyQuote = 'Shit\'s gas, on god fr';
+
+DiningHall chosenHall = cafeThree;
+
+List<DiningHall> unsortedHalls = [clarkKerr, cafeThree, foothill, crossroads];
+List<DiningHall> halls = sortHalls(unsortedHalls);
 List<String> stars = ['★', '★★', '★★★', '★★★★', '★★★★★'];
 
 void main() {
@@ -64,30 +88,42 @@ class HomeRoute extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const double buttonFontSize = 30;
+    const double nameButtonFontSize = 30;
+    const double openButtonFontSize = 25;
     Widget homeBlock(DiningHall hall, int chosenColor) {
       return Container(
-        height: 200,
+        height: 175,
         width: double.infinity,
         color: Color(chosenColor),
         padding: const EdgeInsets.all(30),
-        child: Align(
-          alignment: const Alignment(-1, 0),
-          child: Column(
-            children: [
-              Text(
+        child: Column(
+          children: [
+            Align(
+              alignment: const Alignment(-1, 0),
+              child: Text(
                 hall.name,
-                style: const TextStyle(fontSize: buttonFontSize),
+                style: const TextStyle(
+                    fontSize: nameButtonFontSize, fontWeight: FontWeight.bold),
               ),
-              Text(
+            ),
+            Align(
+              alignment: const Alignment(-1, 0),
+              child: Text(
                 (hall.dailyRating > 0)
                     ? stars[hall.dailyRating.round() - 1]
                     : 'No Reviews Yet',
-                style: const TextStyle(
-                    color: Colors.yellow, fontSize: buttonFontSize),
+                style: TextStyle(
+                    color:
+                        (hall.dailyRating > 0) ? Colors.yellow : Colors.black,
+                    fontSize: nameButtonFontSize),
               ),
-            ],
-          ),
+            ),
+            Text((hall.isOpen) ? 'OPEN' : 'CLOSED',
+                style: TextStyle(
+                    fontSize: openButtonFontSize,
+                    fontWeight: FontWeight.w700,
+                    color: (hall.isOpen) ? Colors.green : Colors.red)),
+          ],
         ),
       );
     }
@@ -97,10 +133,23 @@ class HomeRoute extends StatelessWidget {
       appBar: AppBar(
         title: const Text('FoodGrind'),
         backgroundColor: const Color(0xFF2F80EC),
+        shadowColor: const Color(0x00FFFFFF),
       ),
       body: Stack(children: <Widget>[
         Column(
           children: <Widget>[
+            Container(
+              color: const Color(0xFF2F80EC),
+              height: 75,
+              width: double.infinity,
+              padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+              child: Center(
+                child: Text(
+                  dailyQuote,
+                  style: const TextStyle(fontSize: 20, color: Colors.white),
+                ),
+              ),
+            ),
             GestureDetector(
                 onTap: () {
                   chosenHall = halls[0];
